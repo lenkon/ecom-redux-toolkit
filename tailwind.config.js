d694086194f25db8f4a08b4d39877accc4681257
inoulen inoulen <inoulenkon@gmail.com>
2024-01-15 19:18:41 +0530

    install material tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,23 @@
 //   plugins: [],
 // }
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
