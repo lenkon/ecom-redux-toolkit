00a4b5180fdbcb94392d468a06ac626101407aa7
inoulen inoulen <inoulenkon@gmail.com>
2024-01-18 23:46:07 +0530

    Make slide data length to be dynamic and display carousel images

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { nextSlide, prevSlide, dotSlide } from '../../features/slices/sliderSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import { sliderData } from '../../assets/data/dummyData';
 
 const Slider = () => {
   const slideIndex = useSelector((state) => state.slider.value);
   console.log('slideIndex', slideIndex);  
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className='relative pb-4'>
+      <div>
+        {sliderData.map((item) => {
+          return <div key={item.id} 
+          className={parseInt(item.id) === slideIndex ? 
+            "opacity-100 duration-700 ease-in-out scale-100" :
+            "opacity-0 duration-700 ease-in-out scale-95"
+            }>
+            <div>
+              {parseInt(item.id) === slideIndex && (
+              <img className='' src={item.img} alt='shoes'></img>
+              )}
+            </div>
+            <div>
+              <p className='text-4xl font-inter font-bold tracking-normal leading-none'>
+                {parseInt(item.id) === slideIndex && item.text}
+              </p>
+            </div>
+          </div>
+        })}
+      </div>
       <button onClick={() => dispatch(nextSlide(slideIndex + 1))}>Next</button>
       <button onClick={() => dispatch(prevSlide(slideIndex - 1))}>Prev</button>
     </div>
--- src/features/slices/sliderSlice.jsx
+++ src/features/slices/sliderSlice.jsx
@@ -1,19 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { sliderData } from '../../assets/data/dummyData';
 
 export const sliderSlice = createSlice({
   name: 'slider',
   initialState: {
     value: 0,
-    length: 4,
+    length: sliderData.length,
   },
   reducers: {
     nextSlide(state, action) {
       console.log("action", action.payload);
       console.log("state", state);
-      state.value = action.payload > state.length ? 0 : action.payload;
+      state.value = action.payload > state.length - 1? 0 : action.payload;
     },
     prevSlide(state, action) {
-      state.value = action.payload < 0 ? state.length : action.payload;
+      state.value = action.payload < 0 ? state.length - 1 : action.payload;
     },
     dotSlide() {},
   }
