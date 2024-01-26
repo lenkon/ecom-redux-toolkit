a6b972de3f93e51295cea4d6b0a1eed4739fd99f
inoulen inoulen <inoulenkon@gmail.com>
2024-01-26 21:39:47 +0530

    Add link for filtered products

--- src/components/FilteredProducts/FilteredProducts.jsx
+++ src/components/FilteredProducts/FilteredProducts.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 
 const FilteredProducts = () => {
+  const products = useSelector((state) => state.products.FilteredProducts);
   return (
     <div>FilteredProducts</div>
   )
--- src/components/NavigateButtons/NavigateButtons.jsx
+++ src/components/NavigateButtons/NavigateButtons.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import clothes from '../../assets/images/clothes.jpg';
 import { filteredProducts } from '../../features/slices/productsSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const NavigateButtons = () => {
   const buttons = [
@@ -24,19 +25,21 @@ const NavigateButtons = () => {
           buttons.map((button, index) => {
             return (
               <div key={index} className='mr-4'>
-                <ButtonGroup color='blue-gray'>
-                  <Button 
-                    size='md'  
-                    variant='outlined' 
-                    ripple={true} 
-                    className='hover:bg-green-900 duration-300 ease-in-out'
-                    onClick={() => dispatch(filteredProducts(button))}
-                  >
-                    {
-                      button
-                    }
-                  </Button>
-                </ButtonGroup>                
+                <Link to={"filteredProducts/" + button}>
+                  <ButtonGroup color='blue-gray'>                
+                    <Button 
+                      size='md'  
+                      variant='outlined' 
+                      ripple={true} 
+                      className='hover:bg-green-900 duration-300 ease-in-out'
+                      onClick={() => dispatch(filteredProducts(button))}
+                    >
+                      {
+                        button
+                      }
+                    </Button>
+                  </ButtonGroup>               
+                </Link>                   
               </div>
             );
           })}
