6fd8a9e9e9f093f684fde062df7391523ac57920
inoulen inoulen <inoulenkon@gmail.com>
2024-01-27 18:37:43 +0530

    Create ProductCard component

--- src/components/FilteredProducts/FilteredProducts.jsx
+++ src/components/FilteredProducts/FilteredProducts.jsx
@@ -4,9 +4,22 @@ import { useParams } from 'react-router-dom';
 
 
 const FilteredProducts = () => {
-  const products = useSelector((state) => state.products.FilteredProducts);
+  const products = useSelector((state) => state.products.filteredProducts);
+  console.log("products:", products);
+  const {type} = useParams();
+  console.log("type:", type);
+
   return (
-    <div>FilteredProducts</div>
+    <div>
+      <div className="pt-16">
+        <div className="pl-14">
+          <h1 className='text-4xl font-inter text-gray-600 font-bold tracking-normal leading-none'>
+            {type}
+          </h1>
+        </div>
+        <div className="grid grid-cols-4 justify-items-center py-8 gap-12"></div>
+      </div>
+    </div>
   )
 }
 
