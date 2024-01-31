57c25500b26b1b0f9fa652c244a3d7b9c14e2be7
inoulen inoulen <inoulenkon@gmail.com>
2024-01-31 18:35:11 +0530

    Display product name and description

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -18,6 +18,13 @@ const SingleProduct = () => {
             <div className="pl-44 grow-[2]">
               <img className='h-[850px] rounded-lg' src={el.img} alt={el.name} />
             </div>
+            <div className="grow-[3]">
+              <div className="max-w-lg">
+                <h5 className="text-2xl font-inter font-bold tracking-normal leading-none">{el.name}</h5>
+                <p className="text-orange-700 text-xl font-inter font-bold tracking-normal leading-none pb-4">15% OFF</p>
+                <p className="text-gray-600 text-xl font-inter font-bold tracking-normal leading-none pb-4">{el.text}</p>
+              </div>
+            </div>
           </div>
         })
       }
