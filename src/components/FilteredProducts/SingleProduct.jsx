9150bf4541615adb6ad48714aab711309d2086b3
inoulen inoulen <inoulenkon@gmail.com>
2024-02-02 20:46:57 +0530

    Add condition and disabled select option for products without sizes

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -26,21 +26,40 @@ const SingleProduct = () => {
                 <p className="text-orange-700 text-xl font-inter font-bold tracking-normal leading-none pb-4">15% OFF</p>
                 <p className="text-gray-600 text-xl font-inter font-bold tracking-normal leading-none pb-4">{el.text}</p>
                 <div className="pb-4">
-                  <div className="">
-                    <label htmlFor="size" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white pb-4'>Pick a size</label>
-                    <select 
-                      name="size" 
-                      id="size" 
-                      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'>
-                      value={size}
-                      onChange{(e) => setSize(e.target.value)}
-                      {el.size.map((item, index) => {
-                        return(
-                          <option value={item} key={index}>{item}</option>
-                        )                        
-                      })}
-                    </select>
-                  </div>
+                  {item.size ? (
+                    <div className="">
+                      <label htmlFor="size" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white pb-4'>Pick a size</label>
+                      <select 
+                        name="size" 
+                        id="size" 
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'>
+                        value={size}
+                        onChange{(e) => setSize(e.target.value)}
+                        {el.size.map((item, index) => {
+                          return(
+                            <option value={item} key={index}>{item}</option>
+                          )                        
+                        })}
+                      </select>
+                    </div>
+                  ) : (
+                    <div className="">
+                      <label htmlFor="size" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white pb-4'>Pick a size</label>
+                      <select 
+                        name="size" 
+                        id="size" 
+                        disabled={true}
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'>
+                        value={size}
+                        onChange{(e) => setSize(e.target.value)}
+                        {el?.size?.map((item, index) => {
+                          return(
+                            <option value={item} key={index}>{item}</option>
+                          )                        
+                        })}
+                      </select>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
