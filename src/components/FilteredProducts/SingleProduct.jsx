e93c5ecca337d4baec6133894a9a9cccde6ede93
inoulen inoulen <inoulenkon@gmail.com>
2024-02-03 17:44:42 +0530

    Add color select option

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -8,7 +8,8 @@ const SingleProduct = () => {
   const productSize = product[0].size[0] ? product[0].size[0] : "";
   const { id } = useParams();
   const [size, setSize] = useState(productSize);
-
+  const productColor = product[0].size[0];
+  const [color, setColor] = useState(productColor);
   return (
     <div>
       {
@@ -26,7 +27,7 @@ const SingleProduct = () => {
                 <p className="text-orange-700 text-xl font-inter font-bold tracking-normal leading-none pb-4">15% OFF</p>
                 <p className="text-gray-600 text-xl font-inter font-bold tracking-normal leading-none pb-4">{el.text}</p>
                 <div className="pb-4">
-                  {item.size ? (
+                  {el.size ? (
                     <div className="">
                       <label htmlFor="size" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white pb-4'>Pick a size</label>
                       <select 
@@ -61,6 +62,23 @@ const SingleProduct = () => {
                     </div>
                   )}
                 </div>
+                {/* <div className=""> */}
+                  <div className="">
+                    <label htmlFor="color" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white pb-4'>Pick a color</label>
+                    <select 
+                      name="color" 
+                      id="color" 
+                      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'>
+                      value={color}
+                      onChange{(e) => setColor(e.target.value)}
+                      {el.color.map((item, index) => {
+                        return(
+                          <option value={item} key={index}>{item}</option>
+                        )                        
+                      })}
+                    </select>
+                  </div>
+                {/* </div> */}
               </div>
             </div>
           </div>
