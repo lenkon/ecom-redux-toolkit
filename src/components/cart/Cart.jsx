995edd4b208def3ab36eb70f586c39084b676398
inoulen inoulen <inoulenkon@gmail.com>
2024-02-13 19:12:17 +0530

    Add condition for cart display

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -6,27 +6,53 @@ import {
   DialogBody,
   DialogFooter,
 } from "@material-tailwind/react";
+import { Tooltip } from "@material-tailwind/react";
+import { useSelector, useDispatch } from 'react-redux';
 
 const Cart = ({ openModal, setOpen }) => {
+  const cart = useSelector((state) => state.cart.cart);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
   return (
     <>
-      <Dialog
-        open={openModal}
-        handler={() => setOpen(false)}
-        animate={{
-          mount: { scale: 1, y: 0 },
-          unmount: { scale: 0.9, y: -100 },
-        }}
-      >
-        <DialogHeader>Its a simple dialog.</DialogHeader>
-        <DialogBody>
-          The key to more success is to have a lot of pillows. Put it this way,
-          it took me twenty five years to get these plants, twenty five years of
-          blood sweat and tears, and I&apos;m never giving up, I&apos;m just
-          getting started. I&apos;m up to something. Fan luv.
-        </DialogBody>
-        {/* <DialogFooter> </DialogFooter> */}
-      </Dialog>
+      {cart.length > 0 ? (
+        <Dialog
+        className='border-0 outline-0'
+          open={openModal}
+          handler={() => setOpen(false)}
+          animate={{
+            mount: { scale: 1, y: 0 },
+            unmount: { scale: 0.9, y: -100 },
+          }}
+        >
+          <DialogHeader>Its a simple dialog.</DialogHeader>
+          <DialogBody>
+            The key to more success is to have a lot of pillows. Put it this way,
+            it took me twenty five years to get these plants, twenty five years of
+            blood sweat and tears, and I&apos;m never giving up, I&apos;m just
+            getting started. I&apos;m up to something. Fan luv.
+          </DialogBody>
+          {/* <DialogFooter> </DialogFooter> */}
+        </Dialog>
+      ) : (
+        <Dialog
+          className='border-0 outline-0'
+          open={openModal}
+          handler={() => setOpen(false)}
+          animate={{
+            mount: { scale: 1, y: 0 },
+            unmount: { scale: 0.9, y: -100 },
+          }}
+        >
+          <DialogHeader>Its a simple dialog.</DialogHeader>
+          <DialogBody>
+            The key to more success is to have a lot of pillows. Put it this way,
+            it took me twenty five years to get these plants, twenty five years of
+            blood sweat and tears, and I&apos;m never giving up, I&apos;m just
+            getting started. I&apos;m up to something. Fan luv.
+          </DialogBody>
+          {/* <DialogFooter> </DialogFooter> */}
+        </Dialog>
+      )}
     </>
   )
 }
