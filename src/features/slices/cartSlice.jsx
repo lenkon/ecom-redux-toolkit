b0942b0b55895f0e8ba0455513bd10f256d04234
inoulen inoulen <inoulenkon@gmail.com>
2024-02-16 11:21:36 +0530

    Add reducer to remove product from cart

--- src/features/slices/cartSlice.jsx
+++ src/features/slices/cartSlice.jsx
@@ -42,8 +42,36 @@ export const cartSlice = createSlice ({
         return err;
       }
     },
+
+    removeFromCart (state, action) {
+      const productId = action.payload;
+      try {
+        const exist = state.cart.find(
+          (product) => product.id === productId.id && 
+          product.size === productId.size && 
+          product.color === productId.color
+        );
+
+        if(exist.amount === 1) {
+          state.cart = state.cart.filter((product) => 
+            product.id !== productId.id ||
+            product.size !== productId.size ||
+            product.color !== productId.color
+          );
+          state.totalAmount--;
+          state.totalPrice -= productId.price;
+        } else {
+          exist.amount--;
+          exist.totalPrice -= productId.price;
+          state.totalAmount--;
+          state.totalPrice -= productId.price;
+        }
+      } catch(err) {
+        return err;
+      }
+    }
   }
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
