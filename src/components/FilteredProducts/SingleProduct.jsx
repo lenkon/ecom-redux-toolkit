13ab4c980ead3484ea74a5eb660643cd2edd0f37
inoulen inoulen <inoulenkon@gmail.com>
2024-02-19 10:24:11 +0530

    Fix size display error

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -37,9 +37,10 @@ const SingleProduct = () => {
                       <select 
                         name="size" 
                         id="size" 
-                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'>
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'
                         value={size}
-                        onChange{(e) => setSize(e.target.value)}
+                        onChange = {(e) => setSize(e.target.value)}
+                      >
                         {el.size.map((item, index) => {
                           return(
                             <option value={item} key={index}>{item}</option>
@@ -54,9 +55,10 @@ const SingleProduct = () => {
                         name="size" 
                         id="size" 
                         disabled={true}
-                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'>
+                        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pb-4'
                         value={size}
-                        onChange{(e) => setSize(e.target.value)}
+                        onChange = {(e) => setSize(e.target.value)}
+                      >
                         {el?.size?.map((item, index) => {
                           return(
                             <option value={item} key={index}>{item}</option>
