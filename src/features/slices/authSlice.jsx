4c88cec1b7cd541dfc6a4d780b02a25c00d09263
inoulen inoulen <inoulenkon@gmail.com>
2024-03-01 23:08:12 +0530

    Define the login reducer

--- src/features/slices/authSlice.jsx
+++ src/features/slices/authSlice.jsx
@@ -12,7 +12,19 @@ export const authSlice = createSlice({
   },
 
   reducers: {
-    login(state, action) {},
+    login(state, action) {
+      const userId = action.payload;
+      const userValidation = /^[A-Za-z]{4,10}$/i.test(userId.name);
+      const passwordValidation = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,10}$/i.test(userId.password);
+      state.user = userId;
+      if(!userValidation || !passwordValidation) {
+        state.user.authUser = false;
+      } else {
+        state.user.authUser = true;
+        const saveState = JSON.stringify(userId);
+        sessionStorage.setItem("authUser", saveState);
+      }
+    },
     logout(state) {},
   },
 });
