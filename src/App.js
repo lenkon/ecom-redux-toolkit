4fe0ac22964e46d30141c289bc83e312b645e488
inoulen inoulen <inoulenkon@gmail.com>
2024-03-03 23:23:56 +0530

    Add the login authentication functionality to the app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import { useSelector } from 'react-redux';
 import Login from './components/login/Login';
 
 function App() {
+  const user = useSelector((state) => state.user.user);
+  const { authUser } = user;
+  console.log("user", user);
+  console.log("authUser", authUser);
   // const cart = useSelector((state) => state.cart.cart);
   // const totalAmount = useSelector((state) => state.cart.totalAmount);
   // const totalPrice = useSelector((state) => state.cart.totalPrice);
@@ -20,8 +24,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/login' element={<Login />} />
+          <Route 
+            path='/' 
+            element={authUser ? <Main /> : <Login />} 
+          />
+          {/* <Route path='/login' element={<Login />} /> */}
           <Route path='/filteredProducts/:type' element={<FilteredProducts />} />
           <Route path='/filteredProducts/:type/:id' element={<SingleProduct />} />       
         </Routes>
